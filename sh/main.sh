cac80e58b59199d11b3db8803ed44f889cc59704
GhostyTongue <50715350+Ghosty-Tongue@users.noreply.github.com>
2024-04-01 12:19:10 -0700

    Update main.sh

--- sh/main.sh
+++ sh/main.sh
@@ -2,43 +2,54 @@
 
 download_podcasts() {
     feed_url=$1
+    folder_name=$2
+    max_episodes=${3:-9999999}
 
-    if [ -z "$feed_url" ]; then
-        echo "Error: Feed URL must be specified."
+    if [ -z "$feed_url" ] || [ -z "$folder_name" ]; then
+        echo "Error: Feed URL and folder name must be specified."
         return
     fi
 
-    if [ ! -f "podarchive.txt" ]; then
-        touch "podarchive.txt"
-    fi
-
-    feed_xml=$(curl -s "$feed_url")
-
-    domain=$(echo "$feed_url" | awk -F/ '{print $3}')
-    folder_path="$HOME/Downloads/$domain"
+    folder_path=$(realpath "$folder_name")
 
     if [ ! -d "$folder_path" ]; then
         mkdir -p "$folder_path"
     fi
 
-    for enclosure in $(echo "$feed_xml" | grep -o '<enclosure url="[^"]*'); do
-        url=$(echo "$enclosure" | sed 's/<enclosure url="//' | sed 's/"//')
-        title=$(echo "$enclosure" | grep -o 'title=".*"' | sed 's/title="//' | sed 's/"//')
+    feed_xml=$(curl -s "$feed_url")
 
-        if [ -f "$folder_path/${title}.mp3" ]; then
+    url_list=($(echo "$feed_xml" | grep -o '<enclosure url="[^"]*' | grep -o 'url="[^"]*' | cut -d'"' -f2))
+    title_list=($(echo "$feed_xml" | grep -o '<title>.*</title>' | sed -e 's/<title>//' -e 's/<\/title>//'))
+    guid_list=($(echo "$feed_xml" | grep -o '<guid>.*</guid>' | sed -e 's/<guid>//' -e 's/<\/guid>//'))
+
+    for ((i = 0; i < ${#url_list[@]} && i < max_episodes; i++)); do
+        url="${url_list[i]}"
+        title="${title_list[i]}"
+        guid="${guid_list[i]}"
+
+        after_slash=$(basename "$url")
+        file_name=$(echo "$after_slash" | cut -d'?' -f1)
+        file_extension=$(echo "$file_name" | rev | cut -d'.' -f1 | rev)
+
+        if grep -qFx "$feed_url • $guid" podarchive.txt; then
             echo "Skipping episode \"$title\" because it has already been downloaded."
         else
-            if curl -s "$url" -o "$folder_path/${title}.mp3"; then
-                echo "$feed_url • $title" >> "$folder_path/podarchive.txt"
+            if curl -s -o "$folder_path/$title.$file_extension" "$url"; then
+                echo "$feed_url • $guid" >>podarchive.txt
             else
                 echo "Error downloading episode \"$title\"."
             fi
+        fi
+
+        if [ "$i" -ge "$max_episodes" ]; then
+            break
         fi
     done
 
     echo "All downloads complete."
 }
 
+read -p "Enter the folder name where files will be saved: " folder_name
 read -p "Enter the RSS feed URL: " feed_url
 
-download_podcasts "$feed_url"
+download_podcasts "$feed_url" "$folder_name"
